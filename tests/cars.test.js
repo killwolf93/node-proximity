1e14377486dc598e62a214684390ec93eed6e70d
andresmata <amata@xtensifi.com>
2020-07-21 16:48:05 -0600

    Adding success scenario in unit test.

--- tests/cars.test.js
+++ tests/cars.test.js
@@ -1,16 +1,12 @@
 const request = require('supertest');
 const App = require('../src/app');
+const MOCKS = require("./mocks/mocks");
+const filePath = `${__dirname}/mocks/proxiCar.csv`;
 let app;
 
-beforeAll(async (done) => {
-  app = await App.getApp();
-  done()
-});
 
-afterAll(async (done) => {
-  await App.closeDB()
-  done();
-});
+beforeAll(async () => app = await App.getApp());
+afterAll(async () => await App.closeDB());
 
 describe('Upload Test', () => {
   test('It should validate empty POST request', async () => {
@@ -25,5 +21,20 @@ describe('Upload Test', () => {
         .post('/cars')
         .send({provider: 'test'})
         .expect(400);
+  })
+
+  test('It should insert the car info as expected', async () => {
+    await request(app)
+        .post('/cars')
+        .field("provider", "proxiCar")
+        .attach('file', filePath)
+        .expect(200);
+
+    const expected = MOCKS.expected;
+    await request(app)
+        .get('/cars')
+        .expect(200).then(response => {
+          expect(response.body.data[0].VIN).toEqual(expected.VIN);
+        })
   })
 })
